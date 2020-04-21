0d64bc745139435ef489e237419b800cf1de6ca9
AryaMedapati <42080625+AryaMedapati@users.noreply.github.com>
2020-04-20 22:57:26 -0700

    Update HeatMap.py

--- HeatMap.py
+++ HeatMap.py
@@ -3,7 +3,10 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
 
-def HeatMap(score, tickerSymbol, cmap, bestScore, bestTs):
+def HeatMap(score, tickerSymbol, cmap, bestScore, bestTs, vmin, vmax):
+
+
+
     print("score, tickerSYmbol, cmap, bestscore, bestTs")
     print(score)
     print(tickerSymbol)
@@ -11,46 +14,43 @@ def HeatMap(score, tickerSymbol, cmap, bestScore, bestTs):
     print(bestScore)
     print(bestTs)
     fig, ax3 = plt.subplots(figsize=(13, 1.3))
-    fig.subplots_adjust(bottom=0.5)
 
 
-    if score < 0:
-        score = 0 - score
-        final = tickerSymbol + " Negative " + str(round(score, 2))
-    else:
-        final = tickerSymbol + " " + str(round(score, 2))
+    final = tickerSymbol + " " + str(round(score, 2))
 
     # cmap = matplotlib.cm.cool
     # labels = ('Dreadful', 'Poor', 'Mediocre', 'Good', 'Excellent', 'Phenomenal')
-    norm = matplotlib.colors.Normalize(vmin=0 ,vmax=100)
-    trans = ax3.get_yaxis_transform()
+    norm = matplotlib.colors.Normalize(vmin=vmin ,vmax=vmax)
+    matplotlib.colorbar.ColorbarBase(ax3, cmap=cmap, norm=norm, orientation='horizontal')
+    #ax3.set_xlabel('Correlation', fontsize=14, color='b')
+
+    fig.subplots_adjust(bottom=0.5)
 
     print("score and bestScore")
     print(score)
     print(bestScore)
 
+    down = vmin -1.25 * (vmax-vmin)
+
     if tickerSymbol != bestTs:
-        ax3.annotate(final, xy=(score/100,-0.3),
-                        xytext=(score/100, -1.23), xycoords=trans, textcoords='axes fraction', color='tab:blue',
+        ax3.annotate(final, xy=(score, vmin),
+                    xytext=(score, down), xycoords='data', textcoords='data', color='tab:blue',
         arrowprops=dict(color='tab:blue', arrowstyle='->'),
         horizontalalignment='center', verticalalignment='baseline',)
+
 
     #color='tab:blue', shrink=0.0, width=0.5, headwidth=3.5, headlength=3.5
     #color='green', shrink=0.0, width=0.5, headwidth=3.5, headlength=3.5
 
 
-    if bestScore < 0:
-        bestScore = 0 - bestScore
-        final2 = "Best: " + bestTs + " Negative " + str(round(bestScore, 2))
-    else:
-        final2 = "Best: " + bestTs + " " + str(round(bestScore, 2))
 
-    ax3.annotate(final2, xy=(bestScore/100,-0.3),
-                        xytext=(bestScore/100, -0.98), xycoords=trans, textcoords='axes fraction', color='green',
+    final2 = "Best: " + bestTs + " " + str(round(bestScore, 2))
+
+    ax3.annotate(final2, xy=(bestScore,vmin),
+                        xytext=(bestScore, vmin-(vmax-vmin)), xycoords='data', textcoords='data', color='green',
     arrowprops=dict(color='green', arrowstyle='->'),
     horizontalalignment='center', verticalalignment='baseline',)
-    ax3.set_xlabel('Correlation', fontsize=14, color='b')
-    matplotlib.colorbar.ColorbarBase(ax3, cmap=cmap, norm=norm, orientation='horizontal')
+
     #cb1.ax.set_xticklabels(labels)
     img1 = BytesIO()
     plt.savefig(img1, format='png')
