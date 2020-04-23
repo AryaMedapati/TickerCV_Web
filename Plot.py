a105358b8331a8277cd5f2a97eb068d9f358a374
AryaMedapati <42080625+AryaMedapati@users.noreply.github.com>
2020-04-23 09:33:22 -0700

    Update Plot.py

--- Plot.py
+++ Plot.py
@@ -6,7 +6,7 @@ from io import BytesIO
 import base64
 
 def Plot(sDates1, sCloses, tickerSymbol, tCases):
-    fig, ax1= plt.subplots(figsize=(10, 4.5))
+    fig, ax1= plt.subplots(figsize=(9, 4.5))
     color = 'tab:blue'
     ax1.set_title('Stock: ' + tickerSymbol, fontsize= 16, color='b')
     ax1.set_xlabel('date', fontsize=14, color='b')
@@ -32,7 +32,7 @@ def Plot(sDates1, sCloses, tickerSymbol, tCases):
 
     ax2.tick_params(axis='x', which='major', labelsize=10)
     img = BytesIO()
-    plt.savefig(img, format='png')
+    plt.savefig(img, format='png', bbox_inches='tight', pad_inches = 0.1)
     plt.close()
     img.seek(0)
     plot_url = base64.b64encode(img.getvalue()).decode('utf8')
